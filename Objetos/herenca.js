4c856b8a0c74df287357eb79a6e43844066d4638
Matheusmauriciom <150610041+Matheusmauriciom@users.noreply.github.com>
2023-12-05 09:32:39 -0300

    commit-obj

---
+++ Objetos/herenca.js
@@ -0,0 +1,10 @@
+// extendes para refrir a classe herdada.
+class teste extends Product{
+    constructor(name, price, colors){
+        
+    }
+}
+
+// metodo do objeto date
+const date = new Date()
+console.log(date)
